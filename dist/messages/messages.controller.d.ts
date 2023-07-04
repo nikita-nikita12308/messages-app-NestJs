48510a77c57268d0e990337f538f0813b340b0c7
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-07-04 23:40:27 +0300

    day 5

--- dist/messages/messages.controller.d.ts
+++ dist/messages/messages.controller.d.ts
@@ -1,5 +1,6 @@
+import { CreateMessageDto } from './dtos/create-message.dto';
 export declare class MessagesController {
     listMessage(): void;
-    createMessage(body: any): void;
+    createMessage(body: CreateMessageDto): void;
     getMessage(id: string): void;
 }
